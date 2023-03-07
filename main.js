49202203fb2e20817a2eb31f9d620bb492be6f26
kelvinbest1 <kelvinlanier@yahoo.com>
2023-03-07 15:28:18 -0500

    adding questions to main.js

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 //constants
-const triviaData =[{question:"What’s the diameter of a basketball hoop in inches?",
+const triviaData =[{question:"What's the diameter of a basketball hoop in inches?",
 a: "34 inches",
 b: "23 inches",
 c: "22 inches",
 d: "18 inches",
 correct: "d",},
-{}
+{question:"The Olympics are held every how many years?",
+a: "4 years",
+b: "9 years",
+c: "12 years",
+d: "7 years.",
+correct: "a",},
+{question: " What sport is best known as the ‘king of sports’?",
+a: "football",
+b: "basketball",
+c: "soccer",
+d: "hockey",
+correct: "c",},
 ]
 
 //state variables
@@ -21,3 +32,5 @@ const b_text = document.getElementById("b_text")
 const c_text = document.getElementById("c_text")
 const d_text = document.getElementById("d_text")
 const submitButton = document.getElementById("submit")
+
+// event listeners
