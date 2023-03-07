cf18154a23dc46cdcb0764a9e1853071f8273cac
kelvinbest1 <kelvinlanier@yahoo.com>
2023-03-07 13:54:27 -0500

    adding cached elements to main.js

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const triviaData =[{}]
 //state variables
 let score;
 let quizCurrent;
+
+// cached elements
+const quiz = document.getElementById("quiz")
+const questionElement = document.getElementById("question")
+const a_text = document.getElementById("a_text")
+const 
